87a8105fa5ea3de5d7081df4135c6c0b1cb94c4f
joey <joey@mail>
2024-07-17 13:37:52 +0800

    Let every level have {Level}Contextf

--- depthlogger.go
+++ depthlogger.go
@@ -28,6 +28,10 @@ func (o *DepthLogger) DebugDepthf(msg string, skip int, args ...any) {
 	o.LogDepthf(context.Background(), slog.LevelDebug, skip+1, msg, args...)
 }
 
+func (o *DepthLogger) DebugContextf(ctx context.Context, msg string, args ...any) {
+	o.LogDepthf(ctx, slog.LevelDebug, 1, msg, args...)
+}
+
 func (o *DepthLogger) DebugContextDepth(ctx context.Context, skip int, msg string, args ...any) {
 	o.LogDepth(ctx, slog.LevelDebug, skip+1, msg, args...)
 }
@@ -46,6 +50,10 @@ func (o *DepthLogger) InfoDepthDepth(skip int, msg string, args ...any) {
 
 func (o *DepthLogger) InfoDepthDepthf(skip int, msg string, args ...any) {
 	o.LogDepthf(context.Background(), slog.LevelInfo, skip+1, msg, args...)
+}
+
+func (o *DepthLogger) InfoContextf(ctx context.Context, msg string, args ...any) {
+	o.LogDepthf(ctx, slog.LevelInfo, 1, msg, args...)
 }
 
 func (o *DepthLogger) InfoContextDepth(ctx context.Context, skip int, msg string, args ...any) {
@@ -68,6 +76,10 @@ func (o *DepthLogger) WarnDepthf(skip int, msg string, args ...any) {
 	o.LogDepthf(context.Background(), slog.LevelWarn, skip+1, msg, args...)
 }
 
+func (o *DepthLogger) WarnContextf(ctx context.Context, msg string, args ...any) {
+	o.LogDepthf(ctx, slog.LevelWarn, 1, msg, args...)
+}
+
 func (o *DepthLogger) WarnContextDepth(ctx context.Context, skip int, msg string, args ...any) {
 	o.LogDepth(ctx, slog.LevelWarn, skip+1, msg, args...)
 }
@@ -86,6 +98,10 @@ func (o *DepthLogger) ErrorDepth(skip int, msg string, args ...any) {
 
 func (o *DepthLogger) ErrorDepthf(skip int, msg string, args ...any) {
 	o.LogDepthf(context.Background(), slog.LevelError, skip+1, msg, args...)
+}
+
+func (o *DepthLogger) ErrorContextf(ctx context.Context, msg string, args ...any) {
+	o.LogDepthf(ctx, slog.LevelError, 1, msg, args...)
 }
 
 func (o *DepthLogger) ErrorContextDepth(ctx context.Context, skip int, msg string, args ...any) {
